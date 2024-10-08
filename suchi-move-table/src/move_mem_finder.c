57b30570e9e5cd8b9418d2cb96728597fb67cd34
hamza wahed <hzd454@gmail.com>
2024-10-07 22:38:30 -0300

    Travis move table mem finding

--- suchi-move-table/src/move_mem_finder.c
+++ suchi-move-table/src/move_mem_finder.c
@@ -1,12 +1,11 @@
 #include "file_utils.h"
-#include "move_table.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_PATTERN_LENGTH 1000000
-#define MIN_MEM_LENGTH 40
+#define MIN_MEM_LENGTH 10
 
 #define FORWARD 1
 #define BACKWARD -1
@@ -14,20 +13,57 @@
 #define SLTEE(s_run, s_offset, e_run, e_offset)                                \
     ((s_run) < (e_run) || ((s_run) == (e_run) && (s_offset) <= (e_offset)))
 
+typedef struct row {
+    char head;
+    unsigned char length;
+    int pointer;
+    unsigned char offset;
+} move_table_t;
+
+typedef struct props {
+    move_table_t *table;
+    int r;
+    int n;
+} props;
+
+props read_mvt(char *filename) {
+    FILE *move = fopen(filename, "rb");
+
+    int n;
+    fread(&n, sizeof(int), 1, move);
+    fprintf(stderr, "read n = %i\n", n);
+
+    int r;
+    fread(&r, sizeof(int), 1, move);
+    fprintf(stderr, "read r = %i\n", r);
+
+    move_table_t *table = (move_table_t *)malloc(r * sizeof(move_table_t));
+
+    for (int i = 0; i < r; i++) {
+        fread(&table[i].head, 1, 1, move);
+        fread(&table[i].length, 1, 1, move);
+        fread(&table[i].pointer, sizeof(int), 1, move);
+        fread(&table[i].offset, 1, 1, move);
+    }
+
+    fclose(move);
+
+    fprintf(stderr, "loaded table\n");
+
+    return (props){.table = table, .r = r, .n = n};
+}
+
 // direction = 1 -> right (forward)
 // direction = -1 -> left (backward)
 // I'm going to assume you gave me the right fm index
 // returns the steps I was able to walk
-int forward_backward(move_table_t *mt, int64_t start, char *pattern,
-                     size_t pattern_size, int8_t direction) {
+int forward_backward(move_table_t *mtb, int mt_length, int64_t start,
+                     char *pattern, size_t pattern_size, int8_t direction) {
 
     int64_t s_run = 0;
     int16_t s_offset = 0;
-    int64_t e_run = mt->length - 1;
-    int16_t e_offset = mt->table[e_run].length - 1;
-
-    int64_t mt_len = mt->length;
-    move_table_row_t *mtb = mt->table;
+    int64_t e_run = mt_length - 1;
+    int16_t e_offset = mtb[e_run].length - 1;
 
     int pi = start;
     int counter = 0;
@@ -41,7 +77,7 @@ int forward_backward(move_table_t *mt, int64_t start, char *pattern,
         while (mtb[s_run].head != pattern[pi]) {
             s_run++;
             s_offset = 0;
-            assert(s_run < mt_len);
+            assert(s_run < mt_length);
         }
 
         // if head char != looking char, move e up table
@@ -56,7 +92,7 @@ int forward_backward(move_table_t *mt, int64_t start, char *pattern,
 
         // jump for s using move table
         s_offset += mtb[s_run].offset;
-        s_run = mtb[s_run].ptr;
+        s_run = mtb[s_run].pointer;
 
         // while s is off the current run, move to next run
         while (s_offset >= mtb[s_run].length) {
@@ -66,7 +102,7 @@ int forward_backward(move_table_t *mt, int64_t start, char *pattern,
 
         // jump for e using move table
         e_offset += mtb[e_run].offset;
-        e_run = mtb[e_run].ptr;
+        e_run = mtb[e_run].pointer;
 
         // while e is off the current run, move to next run
         while (e_offset >= mtb[e_run].length) {
@@ -84,15 +120,16 @@ int forward_backward(move_table_t *mt, int64_t start, char *pattern,
 /**
  * Find maximal exact matches (MEMs) for a given pattern within a text.
  */
-int find_mems(move_table_t *mt_straight, move_table_t *mt_reversed,
-              char *pattern, size_t pattern_size) {
+int find_mems(move_table_t *mt_straight, int mt_straight_length,
+              move_table_t *mt_reversed, int mt_reversed_length, char *pattern,
+              size_t pattern_size) {
     int64_t mem_start = 0;
     int64_t mem_end = 0;
 
-    printf("HI there!\n");
     while (mem_end <= pattern_size - 1) {
-        int64_t steps_fw = forward_backward(mt_reversed, mem_start, pattern,
-                                            pattern_size, FORWARD);
+        int64_t steps_fw =
+            forward_backward(mt_reversed, mt_reversed_length, mem_start,
+                             pattern, pattern_size, FORWARD);
         mem_end = mem_start + steps_fw;
 
         if (steps_fw >= MIN_MEM_LENGTH) {
@@ -104,8 +141,9 @@ int find_mems(move_table_t *mt_straight, move_table_t *mt_reversed,
             break;
         }
 
-        int64_t steps_bw = forward_backward(mt_straight, mem_end, pattern,
-                                            pattern_size, BACKWARD);
+        int64_t steps_bw =
+            forward_backward(mt_straight, mt_straight_length, mem_end, pattern,
+                             pattern_size, BACKWARD);
         int64_t new_mem_start = mem_end - steps_bw + 1;
 
         mem_start = new_mem_start;
@@ -127,30 +165,31 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    char *bwt_straight_filename = argv[1];
-    char *bwt_reversed_filename = argv[2];
+    props p1 = read_mvt(argv[1]);
+    props p2 = read_mvt(argv[2]);
 
-    Text bwt_straight = file_utils_read(bwt_straight_filename);
-    Text bwt_reversed = file_utils_read(bwt_reversed_filename);
-
-    if (bwt_straight.T == NULL || bwt_reversed.T == NULL) {
-        fprintf(stderr, "Unable to read bwt files.\n");
-        return EXIT_FAILURE;
-    }
-
-    printf("BUILDING STRAIGHT\n");
-    move_table_t *mt_straight =
-        move_table_create(bwt_straight.T, bwt_straight.len);
-    printf("BUILDING REVERSED\n");
-    move_table_t *mt_reversed =
-        move_table_create(bwt_reversed.T, bwt_reversed.len);
-    printf("DONE\n");
-
+    // char *bwt_straight_filename = argv[1];
+    // char *bwt_reversed_filename = argv[2];
+    //
+    // Text bwt_straight = file_utils_read(bwt_straight_filename);
+    // Text bwt_reversed = file_utils_read(bwt_reversed_filename);
+    //
+    // if (bwt_straight.T == NULL || bwt_reversed.T == NULL) {
+    //     fprintf(stderr, "Unable to read bwt files.\n");
+    //     return EXIT_FAILURE;
+    // }
+    //
+    // printf("BUILDING STRAIGHT\n");
+    // move_table_t *mt_straight =
+    //     move_table_create(bwt_straight.T, bwt_straight.len);
+    // printf("BUILDING REVERSED\n");
+    // move_table_t *mt_reversed =
+    //     move_table_create(bwt_reversed.T, bwt_reversed.len);
+    // printf("DONE\n");
+    //
     char *pattern = malloc(MAX_PATTERN_LENGTH * sizeof(char));
     if (!pattern) {
         fprintf(stderr, "Failed to allocate memory for pattern\n");
-        move_table_destroy(mt_straight);
-        move_table_destroy(mt_reversed);
         return EXIT_FAILURE;
     }
 
@@ -158,16 +197,12 @@ int main(int argc, char **argv) {
     if (read_len == 0) {
         fprintf(stderr, "No pattern data read from stdin\n");
         free(pattern);
-        move_table_destroy(mt_straight);
-        move_table_destroy(mt_reversed);
         return EXIT_FAILURE;
     }
 
-    find_mems(mt_straight, mt_reversed, pattern, read_len);
+    find_mems(p1.table, p1.r, p2.table, p2.r, pattern, read_len);
 
     free(pattern);
-    move_table_destroy(mt_straight);
-    move_table_destroy(mt_reversed);
 
     return EXIT_SUCCESS;
 }
