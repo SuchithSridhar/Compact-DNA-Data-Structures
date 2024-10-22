ea561b64e7f43a78a30f9bae2e14757989a06e97
nallam <nr858090@dal.ca>
2024-10-22 17:31:19 -0300

    Added block in main() in memFinder_better that initializes and fills bloom filter
    How long should the bitvectors of the bloom filter be?

--- bml-bloom-kmer/travis/memFinder_better.c
+++ bml-bloom-kmer/travis/memFinder_better.c
@@ -27,7 +27,6 @@ typedef struct range {
   int end;
 } range_t;
 
-// define an array of hash_functions to be used in the bloom filter
 hash_function_t hash_functions[] = {
     {59515879, 95578445, 56887169},
     {65835776, 22503992, 34543247},
@@ -37,28 +36,19 @@ hash_function_t hash_functions[] = {
     {88994189, 67949655, 63991913}
 };
 
-
 range_t *findValidSubstrings();
+void windowLeft(char leftChar, char rightChar, int w);
+int checkFilter(int w);
 
-// === functions for filter ===
-
-void windowLeft(char leftChar, char rightChar);
-int checkFilter();
-
-// ============================
 
 int PATTERN_LENGTH;
 char *PATTERN;
-
 int MIN_MEM_LENGTH;
-
 int k;
 int pow5;
 int insPar;
-
 int window;
 
-// declare bloom filter
 bloom_filter_t* bloom_filter;
 
 
@@ -76,10 +66,6 @@ void printRanges(range_t *ranges) {
 }
 
 range_t *findValidSubstrings() {
-  // create bloom filter
-  bloom_filter = malloc(sizeof(bloom_filter_t));
-  bloom_filter = bf_create(8, hash_functions, 6); 
-
   int x = 0;
   int xPrime = 0;
   range_t *ranges = calloc(RANGES_SIZE, sizeof(range_t));
@@ -92,16 +78,17 @@ range_t *findValidSubstrings() {
     valid_range = 1;
 
     for (int i = x + MIN_MEM_LENGTH - 1; i >= x; i--) {
-      windowLeft(PATTERN[i], (i + k <= x + MIN_MEM_LENGTH - 1 ? PATTERN[i + k] : '#'));
+      windowLeft(PATTERN[i], (i + k <= x + MIN_MEM_LENGTH - 1 ? PATTERN[i + k] : '#'), window);
 
       if (i <= x + MIN_MEM_LENGTH - k &&
           ((94607073 * (long long)window + 54204618) % 55586519) % insPar ==
               0 &&
-          !checkFilter()) {
+          !checkFilter(window)) {
         xPrime = i + 1;
         valid_range = 0;
         break;
       }
+
     }
 
     if (valid_range) {
@@ -113,7 +100,9 @@ range_t *findValidSubstrings() {
         ranges[range_index].end = x + MIN_MEM_LENGTH;
         range_index++;
       }
+
       x++;
+
     } else {
       x = xPrime;
     }
@@ -125,105 +114,116 @@ range_t *findValidSubstrings() {
 
 int main(int argc, char *argv[]) {
 
-  printf("Usage: ./memFinder <pattern file> <k> <insPar> <min_mem_length>\n");
+  printf("Usage: ./memFinder <pattern file> <k> <insPar> <min_mem_length> <text file>\n");
 
-  // load pattern and pattern length
+  // load parameters
   char* patternFileName = argv[1];
   k = atoi(argv[2]);
   insPar = atoi(argv[3]);
   MIN_MEM_LENGTH = atoi(argv[4]);
-
-
+  char* textFileName = argv[5];
   pow5 = 1;
-
   for (int i = 1; i < k; i++) {
     pow5 *= 5;
   }
 
-  FILE *patternFile = fopen("patterns.txt", "r");
+
+  // load text file 
+  FILE *textFile = fopen(textFileName, "r");
+  fseek(textFile, 0, SEEK_END);
+  int n = (int)ftell(textFile);
+  rewind(textFile);
+  char* T = (char *)malloc(n);
+  fread(T, 1, n, textFile);
+  fclose(textFile);
+
+  // create bloom filter ?
+  bloom_filter = bf_create(1000, hash_functions, 6);
+  int createWindow = 0;
+  int tupleCount = 0;
+  for (int i = n - 1; i >= 0; i--) {
+    windowLeft(T[i], (i + k < n ? T[i + k] : '#'), createWindow);
+    if (((94607073 * (long long)createWindow + 54204618) % 55586519) % insPar == 0) {
+      if (!checkFilter(createWindow)) {
+        long long v = (long long)createWindow;
+        bf_insert(bloom_filter, v);
+        tupleCount++;
+      }
+    }
+  }
+  printf("Filter built, %i %i-tuples added to filter.\n", tupleCount, k);
+
+  // reading each pattern and finding valid substrings
+  FILE *patternFile = fopen(patternFileName, "r");
   if (patternFile == NULL) {
       perror("Unable to open pattern file");
       return EXIT_FAILURE;
   }
-
   char *line = NULL;
   size_t len = 0;
   size_t read;
-
-  int currPattern = 0;
-
-  while ((read = getline(&line, &len, patternFile)) != -1) {
-    currPattern++;
-    printf("\n\n======= Pattern %d ========\n\n", currPattern);
-    // Remove newline character if present
+  while ((read = getline(&line, &len, patternFile)) != -1) {    // Remove newline character if present
     if (line[read - 1] == '\n') {
         line[read - 1] = '\0';
         read--; // Adjust the length to exclude the newline character
     }
-
     // Allocate memory for the pattern
     PATTERN = (char *)malloc(read);
     if (PATTERN == NULL) {
         perror("Unable to allocate memory for pattern");
         exit(EXIT_FAILURE);
     }
-
     // Copy the line to the pattern
     strncpy(PATTERN, line, read);
-
     // Set the pattern length
     PATTERN_LENGTH = (int)read;
-
     // Call the function with the pattern and its length
     range_t* ranges = findValidSubstrings();
     printRanges(ranges);
-
   }
 
   fclose(patternFile);
   return 0;
 }
 
-void windowLeft(char leftChar, char rightChar) {
+void windowLeft(char leftChar, char rightChar, int w) {
 
   switch (rightChar) {
   case 'A':
-    window -= 1;
+    w -= 1;
     break;
   case 'C':
-    window -= 2;
+    w -= 2;
     break;
   case 'G':
-    window -= 3;
+    w -= 3;
     break;
   case 'T':
-    window -= 4;
+    w -= 4;
     break;
   }
 
-  window = window / 5;
+  w = w / 5;
 
   switch (leftChar) {
   case 'A':
-    window += 1 * pow5;
+    w += 1 * pow5;
     break;
   case 'C':
-    window += 2 * pow5;
+    w += 2 * pow5;
     break;
   case 'G':
-    window += 3 * pow5;
+    w += 3 * pow5;
     break;
   case 'T':
-    window += 4 * pow5;
+    w += 4 * pow5;
     break;
   }
 
   return;
 }
 
-int checkFilter() {
-    
-    long long v = (long long)window;
-
+int checkFilter(int w) {
+    long long v = (long long)w;
     return bf_query(bloom_filter, v);
 }
