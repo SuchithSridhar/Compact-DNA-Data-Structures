b22de348855f794c71cd2b232878cb531580a0dc
Suchith Sridhar <suchith.sridhar@gmail.com>
2024-10-27 21:00:02 -0300

    feat: finding substrings using kmers (untested)

---
+++ bml-bloom-kmer/code/kmer_substrings.c
@@ -0,0 +1,117 @@
+#include "file_utils.h"
+#include "kmer_filter.h"
+#include "ssvector.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+// TODO: Move to this to use suchi's move table
+typedef struct {
+    char head;
+    unsigned char length;
+    int pointer;
+    unsigned char offset;
+} row_t;
+
+typedef struct {
+    row_t *table;
+    int n;
+    int r;
+} move_table_t;
+*/
+
+/**
+move_table_t *load_move_table(char *table_filename) {
+    FILE *file = fopen(table_filename, "rb");
+
+    if (file == NULL) {
+        return NULL;
+    }
+
+    move_table_t *table = malloc(sizeof(move_table_t));
+
+    fread(&table->n, sizeof(int), 1, file);
+    fread(&table->r, sizeof(int), 1, file);
+
+    table->table = malloc(sizeof(row_t) * table->r);
+
+    for (int i = 0; i < table->r; i++) {
+        fread(&table->table[i].head, sizeof(int8_t), 1, file);
+        fread(&table->table[i].length, sizeof(int8_t), 1, file);
+        fread(&table->table[i].pointer, sizeof(int32_t), 1, file);
+        fread(&table->table[i].offset, sizeof(int8_t), 1, file);
+    }
+
+    fclose(file);
+
+    return table;
+}
+*/
+
+typedef struct range {
+    int start;
+    int end;
+} range_t;
+
+void range_array_print(char *pattern, range_t *range_array, size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        char tmp = pattern[range_array[i].end];
+        pattern[range_array[i].end] = '\0';
+        fprintf(stdout, "%s\n", &pattern[range_array[i].start]);
+        pattern[range_array[i].end] = tmp;
+    }
+}
+
+ssv_vector_t *find_substrings(Text pattern, kmer_filter_t *kmer_filter,
+                              int min_mem_length) {
+    kmer_filter_t *kf = kmer_filter;
+    int init_size = 1024;
+    ssv_vector_t *range_vec = ssv_init(sizeof(range_t), init_size);
+    range_t range;
+
+    size_t start_substring = 0;
+    size_t end_substring = 0;
+
+    for (size_t i = 0; i < pattern.len - kf->kmer_size; i++) {
+        kmer_int_t kmer = kmerf_as_int(kf, pattern.T, i);
+        if (kmerf_should_contain(kf, kmer) && !kmerf_query(kf, kmer)) {
+            // a kmer not found in bloom filter
+            end_substring = i + kf->kmer_size - 1;
+            if (end_substring - start_substring >= min_mem_length) {
+                range.start = start_substring;
+                range.end = end_substring;
+                ssv_push(range_vec, &range);
+            }
+
+            start_substring = end_substring + 1;
+        }
+    }
+
+    return range_vec;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(
+            stderr,
+            "usage: %s <pattern file> [kmer filter file] <min mem length>\n",
+            argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *pattern_file = argv[1];
+    char *kmer_filter_file = argv[2];
+    char *min_mem_len_str = argv[3];
+
+    kmer_filter_t kmer_filter;
+    Text pattern = file_utils_read(pattern_file);
+    int min_mem_length = atoi(min_mem_len_str);
+    kmerf_load_file(&kmer_filter, kmer_filter_file);
+
+    ssv_vector_t *range_vec =
+        find_substrings(pattern, &kmer_filter, min_mem_length);
+    range_array_print(pattern.T, range_vec->array, range_vec->size);
+
+    return EXIT_SUCCESS;
+}
---
+++ bml-bloom-kmer/code/ssvector.c
@@ -0,0 +1,288 @@
+#include "ssvector.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+ssv_vector_t *ssv_init(size_t memb_size, size_t init_size) {
+    ssv_vector_t *vec = malloc(sizeof(ssv_vector_t));
+
+    if (vec == NULL)
+        return NULL;
+
+    vec->size = 0;
+    vec->memb_size = memb_size;
+    vec->capacity = init_size;
+
+    vec->array = malloc(memb_size * init_size);
+    if (vec->array == NULL) {
+        free(vec);
+        return NULL;
+    }
+
+    return vec;
+}
+
+bool ssv_destroy(ssv_vector_t *vec) {
+    if (vec == NULL)
+        return true;
+
+    free(vec->array);
+    free(vec);
+
+    return true;
+}
+
+bool ssv_shrink_to_fit(ssv_vector_t *vec) {
+    if (vec == NULL)
+        return false;
+
+    if (vec->size == vec->capacity)
+        return true;
+
+    void *new_array = realloc(vec->array, vec->size * vec->memb_size);
+    if (new_array) {
+        vec->array = new_array;
+        vec->capacity = vec->size;
+        return true;
+    }
+    return false;
+}
+
+bool ssv_reserve(ssv_vector_t *vec, size_t count) {
+    if (vec == NULL)
+        return false;
+    // Not allowed to reduce the capacity below size.
+    if (count <= vec->size)
+        return false;
+    // We already have the required capacity.
+    if (count <= vec->capacity)
+        return true;
+
+    void *new_array = realloc(vec->array, count * vec->memb_size);
+    if (new_array) {
+        vec->array = new_array;
+        vec->capacity = count;
+        return true;
+    }
+    return false;
+}
+
+ssv_vector_t *ssv_copy(ssv_vector_t *vec) {
+    if (vec == NULL)
+        return NULL;
+
+    ssv_vector_t *new_vec = malloc(sizeof(ssv_vector_t));
+
+    if (!new_vec)
+        return NULL;
+
+    new_vec->array = malloc(vec->capacity * vec->memb_size);
+    if (!new_vec->array) {
+        free(new_vec);
+        return NULL;
+    }
+
+    memcpy(new_vec->array, vec->array, vec->size * vec->memb_size);
+    new_vec->size = vec->size;
+    new_vec->capacity = vec->capacity;
+    new_vec->memb_size = vec->memb_size;
+    return new_vec;
+}
+
+bool ssv_is_empty(ssv_vector_t *vec) {
+    return vec && vec->size == 0;
+}
+
+size_t ssv_delete_range(ssv_vector_t *vec, size_t start, size_t end) {
+    if (!vec || start >= end || end > vec->size) {
+        return 0;
+    }
+    size_t range = end - start;
+    void *start_ptr = (char *)vec->array + start * vec->memb_size;
+    void *end_ptr = (char *)vec->array + end * vec->memb_size;
+    size_t move_count = vec->size - end;
+    memmove(start_ptr, end_ptr, move_count * vec->memb_size);
+    vec->size -= range;
+    return range;
+}
+
+void *ssv_get(ssv_vector_t *vec, size_t index) {
+    if (vec && index < vec->size) {
+        return (void *)((char *)vec->array + (index * vec->memb_size));
+    }
+
+    return NULL;
+}
+
+void *ssv_peek(ssv_vector_t *vec) {
+    if (vec && vec->size > 0) {
+        return (void *)((char *)vec->array +
+                        ((vec->size - 1) * vec->memb_size));
+    }
+
+    return NULL;
+}
+
+bool ssv_push(ssv_vector_t *vec, void *item) {
+    if (vec == NULL || item == NULL)
+        return false;
+    bool success = true;
+    if (vec->size == vec->capacity) {
+        success = _internal_ssv_resize(vec);
+        if (!success)
+            return false;
+    }
+
+    void *dest = (char *)vec->array + (vec->memb_size * vec->size);
+    memcpy(dest, item, vec->memb_size);
+    vec->size++;
+
+    return true;
+}
+
+void *ssv_pop(ssv_vector_t *vec) {
+    if (vec == NULL || vec->size == 0)
+        return NULL;
+
+    void *src = (char *)vec->array + (vec->memb_size * (vec->size - 1));
+    void *elem = malloc(vec->memb_size);
+
+    memcpy(elem, src, vec->memb_size);
+    vec->size--;
+    return elem;
+}
+
+bool ssv_clear(ssv_vector_t *vec) {
+    if (vec == NULL)
+        return false;
+    vec->size = 0;
+    return true;
+}
+
+bool ssv_delete(ssv_vector_t *vec) {
+    if (vec == NULL || vec->size == 0)
+        return false;
+    vec->size--;
+    return true;
+}
+
+bool ssv_delete_at(ssv_vector_t *vec, size_t index) {
+    if (vec == NULL || vec->size == 0 || index >= vec->size) {
+        return false;
+    }
+
+    void *to_delete = (char *)vec->array + (index * vec->memb_size);
+
+    if (index < vec->size - 1) {
+        void *start = (char *)to_delete + vec->memb_size;
+        size_t bytes_to_move = (vec->size - index - 1) * vec->memb_size;
+        memmove(to_delete, start, bytes_to_move);
+    }
+    vec->size--;
+
+    return true;
+}
+
+void *ssv_pop_at(ssv_vector_t *vec, size_t index) {
+    if (vec == NULL || vec->size == 0 || index >= vec->size) {
+        return NULL;
+    }
+
+    void *elem = malloc(vec->memb_size);
+    void *to_delete = (char *)vec->array + (index * vec->memb_size);
+
+    memcpy(elem, to_delete, vec->memb_size);
+
+    if (index < vec->size - 1) {
+        void *start = (char *)to_delete + vec->memb_size;
+        size_t bytes_to_move = (vec->size - index - 1) * vec->memb_size;
+        memmove(to_delete, start, bytes_to_move);
+    }
+    vec->size--;
+    return elem;
+}
+
+bool ssv_push_at(ssv_vector_t *vec, void *item, size_t index) {
+    if (!vec || index > vec->size) {
+        return false;
+    }
+
+    if (vec->size == vec->capacity) {
+        bool success = _internal_ssv_resize(vec);
+        if (!success)
+            return false;
+    }
+
+    if (index < vec->size) {
+        void *start = (char *)vec->array + (index * vec->memb_size);
+        size_t bytes_to_move = (vec->size - index) * vec->memb_size;
+        memmove((char *)start + vec->memb_size, start, bytes_to_move);
+    }
+
+    void *destination = (char *)vec->array + (index * vec->memb_size);
+    memcpy(destination, item, vec->memb_size);
+
+    vec->size++;
+
+    return true;
+}
+
+bool ssv_replace_at(ssv_vector_t *vec, void *item, size_t index) {
+    if (!vec || index > vec->size) {
+        return false;
+    }
+
+    void *start = (char *)vec->array + (index * vec->memb_size);
+    memcpy(start, item, vec->memb_size);
+    return true;
+}
+
+size_t ssv_index_of(ssv_vector_t *vec, void *item) {
+    if (vec == NULL || item == NULL)
+        return SIZE_MAX;
+
+    void *elem;
+    for (size_t i = 0; i < vec->size; i++) {
+        elem = (char *)vec->array + (vec->memb_size * i);
+        if (memcmp(item, elem, vec->memb_size) == 0) {
+            return i;
+        }
+    }
+    return SIZE_MAX;
+}
+
+void ssv_print(ssv_vector_t *vec, void (*print_elem)(void *)) {
+    for (size_t i = 0; i < vec->size; i++) {
+        print_elem((char *)vec->array + (i * vec->memb_size));
+    }
+}
+
+void ssv_sort(ssv_vector_t *vec, int (*compare)(const void *, const void *)) {
+    qsort(vec->array, vec->size, vec->memb_size, compare);
+}
+
+bool _internal_ssv_resize(ssv_vector_t *vec) {
+    if (vec == NULL)
+        return false;
+    size_t new_size = vec->capacity * GROWTH_FACTOR;
+
+    // Something went wrong. Overflow?
+    if (new_size < vec->capacity)
+        return false;
+
+    // Rounding cause the size to remain the same,
+    // it should increase at least by 1.
+    // This also accounts for if capacity is 0. It will update to be 1.
+    if (new_size == vec->capacity) {
+        new_size = new_size + 1;
+    }
+
+    void *new_array = realloc(vec->array, new_size * vec->memb_size);
+    if (new_array == NULL)
+        return false;
+
+    vec->array = new_array;
+    vec->capacity = new_size;
+    return true;
+}
